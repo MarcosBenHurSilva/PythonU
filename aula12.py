c08aac986701944e0b447ee790c702a47f1234ae
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-19 21:15:21 -0300

    Novas commits

---
+++ aula12.py
@@ -0,0 +1,12 @@
+nome = 'Marcos Silva'
+altura = 1.90
+peso = 95
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Marcos tem 1.90 de altura,
+# pesa 95 quilos e seu IMC é
+# 26.315789473684212
