d29969b194308b17707a3dd774b236c6592c4ed1
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-18 22:36:37 -0300

    Modularização - v2

--- aula97.py
+++ aula97.py
@@ -8,11 +8,11 @@
 # O python conhece todos os módulos e pacotes presentes
 # nos caminhos de sys.path
 
-import aula97_m
-from aula97_m import soma, variavel_modulo
+import aula97
+from aula97 import soma, variavel_modulo
 
 # print('Este módulo se chama', __name__)
-print(aula97_m.variavel_modulo)
+print(aula97.variavel_modulo)
 print(variavel_modulo)
 print(soma(2, 3))
-print(aula97_m.soma(2, 3))
+print(aula97.soma(2, 3))
