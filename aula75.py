792920bdfe6228cd1241ed7f82503e1dd9aeb779
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-19 22:41:02 -0300

    Exercícios

--- aula75.py
+++ aula75.py
@@ -11,16 +11,31 @@
 
 # def quadruplicar(numero):
 #     return numero * 4
-def criar_multiplicador(multiplicador):
-    def multiplicar(numero):
-        return numero * multiplicador
+
+# def criar_multiplicador(multiplicador):
+#     def multiplicar(numero):
+#         return numero * multiplicador
+#     return multiplicar
+
+
+# duplicar = criar_multiplicador(2)
+# triplicar = criar_multiplicador(3)
+# quadruplicar = criar_multiplicador(4)
+
+# print(duplicar(2))
+# print(triplicar(2))
+# print(quadruplicar(2))
+
+def numero(num):
+    def multiplicar(multi):
+        return num * multi
+
     return multiplicar
 
+numero_digitado = float(input('Digite o número: '))
+funcao_multiplicar = numero(numero_digitado)
 
-duplicar = criar_multiplicador(2)
-triplicar = criar_multiplicador(3)
-quadruplicar = criar_multiplicador(4)
+multiplicador_digitado = float(input('Digite o multiplicador: '))
+resultado = funcao_multiplicar(multiplicador_digitado)
 
-print(duplicar(2))
-print(triplicar(2))
-print(quadruplicar(2))
+print(f"Resultado da multiplicação: {resultado}")
