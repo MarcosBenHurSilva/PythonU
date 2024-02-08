a00baed321c5c5411c15100a93982e9659e75086
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-02-07 22:36:09 -0300

    dir, hasattr e getattr em Python

--- aula89.py
+++ aula89.py
@@ -1,9 +1,10 @@
 # dir, hasattr e getattr em Python
-string = 'Marcos'
-metodo = 'strip'
+
+string = "Marcos"
+metodo = "strip"
 
 if hasattr(string, metodo):
-    print('Existe upper')
+    print("Existe upper")
     print(getattr(string, metodo)())
 else:
-    print('Não existe o método', metodo)
+    print("Não existe o método", metodo)
