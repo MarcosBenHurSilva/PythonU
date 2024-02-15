6ba529ebd0f11f83b28a306534482c43cc8d9b27
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-02-15 00:25:23 -0300

    try, except, else e finally

--- aula94.py
+++ aula94.py
@@ -1,18 +1,17 @@
 # try, except, else e finally
 # https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
 try:
-    print('ABRIR ARQUIVO')
-    8/0
+    print("ABRIR ARQUIVO")
+    8 / 0
 except ZeroDivisionError as e:
     print(e.__class__.__name__)
     print(e)
-    print('DIVIDIU ZERO')
+    print("DIVIDIU ZERO")
 except IndexError as error:
-    print('IndexError')
+    print("IndexError")
 except (NameError, ImportError):
-    print('NameError, ImportError')
+    print("NameError, ImportError")
 else:
-    print('Não deu erro')
+    print("Não deu erro")
 finally:
-    print('FECHAR ARQUIVO')
-    
+    print("FECHAR ARQUIVO")
