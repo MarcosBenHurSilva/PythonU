ca7dcf711b3c4dbd57f1da3a8a4f69fb2e620f77
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-02-15 22:28:01 -0300

    Módulos padrão do Python (import, from, as e *)

--- aula96.py
+++ aula96.py
@@ -3,32 +3,32 @@
 # inteiro - import nome_modulo
 # Vantagens: você tem o namespace do módulo
 # Desvantagens: nomes grandes
-# import sys
+import sys
 
-# platform = 'A MINHA'
-# print(sys.platform)
-# print(platform)
+platform = "A MINHA"
+print(sys.platform)
+print(platform)
 
 # partes - from nome_modulo import objeto1, objeto2
 # Vantagens: nomes pequenos
 # Desvantagens: Sem o namespace do módulo
-# from sys import exit, platform
+from sys import exit, platform
 
-# print(platform)
+print(platform)
 
 # alias 1 - import nome_modulo as apelido
-# import sys as s
+import sys as s
 
-# sys = 'alguma coisa'
-# print(s.platform)
-# print(sys)
+sys = "alguma coisa"
+print(s.platform)
+print(sys)
 
 
 # alias 2 - from nome_modulo import objeto as apelido
-# from sys import exit as ex
-# from sys import platform as pf
+from sys import exit as ex
+from sys import platform as pf
 
-# print(pf)
+print(pf)
 
 # Vantagens: você pode reservar nomes para seu código
 # Desvantagens: pode ficar fora do padrão da linguagem
@@ -36,7 +36,7 @@
 # má prática - from nome_modulo import *
 # Vantagens: importa tudo de um módulo
 # Desvantagens: importa tudo de um módulo
-# from sys import exit, platform
+from sys import exit, platform
 
-# print(platform)
-# exit()
+print(platform)
+exit()
