75524b0ac2bfc71c5f79240ee31d9a6450c4a2f7
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-02-20 19:53:43 -0300

    aula de Arquitetura de dados

---
+++ crip.py
@@ -0,0 +1,10 @@
+import base64
+import hashlib
+
+text = "Texto a ser codificado em base64. Aumentando o texto para ver a diferença. Continuar aumentando o texto."
+text2 = "Texto a ser codificado (criptografado)."
+xpto = base64.b64encode(text.encode("utf-8"))
+xpto2 = base64.b64encode(text2.encode("utf-8"))
+
+print(text2)
+print(xpto2)
