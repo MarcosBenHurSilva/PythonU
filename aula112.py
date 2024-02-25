533f17ddcb526f2795ee7f70ab541a9870b0e582
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-02-24 23:33:56 -0300

    filter é um filtro funcional

--- aula112.py
+++ aula112.py
@@ -1,20 +1,21 @@
 # filter é um filtro funcional
 def print_iter(iterator):
-    print(*list(iterator), sep='\n')
+    print(*list(iterator), sep="\n")
     print()
 
 
 produtos = [
-    {'nome': 'Produto 5', 'preco': 10.00},
-    {'nome': 'Produto 1', 'preco': 22.32},
-    {'nome': 'Produto 3', 'preco': 10.11},
-    {'nome': 'Produto 2', 'preco': 105.87},
-    {'nome': 'Produto 4', 'preco': 69.90},
+    {"nome": "Produto 5", "preco": 10.00},
+    {"nome": "Produto 1", "preco": 22.32},
+    {"nome": "Produto 3", "preco": 10.11},
+    {"nome": "Produto 2", "preco": 105.87},
+    {"nome": "Produto 4", "preco": 69.90},
+    {"nome": "Produto 5", "preco": 1583.72},
 ]
 
 
 def filtrar_preco(produto):
-    return produto['preco'] > 100
+    return produto["preco"] > 100
 
 
 # novos_produtos = [
@@ -24,7 +25,7 @@ def filtrar_preco(produto):
 novos_produtos = filter(
     # lambda produto: produto['preco'] > 100,
     filtrar_preco,
-    produtos
+    produtos,
 )
 
 
