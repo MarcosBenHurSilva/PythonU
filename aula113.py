3991047dff5efbcd5c4737c2537543ecf88df869
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-02-26 22:21:28 -0300

    reduce - faz a redução de um iterável em um valor

--- aula113.py
+++ aula113.py
@@ -2,11 +2,12 @@
 from functools import reduce
 
 produtos = [
-    {'nome': 'Produto 5', 'preco': 10},
-    {'nome': 'Produto 1', 'preco': 22},
-    {'nome': 'Produto 3', 'preco': 2},
-    {'nome': 'Produto 2', 'preco': 6},
-    {'nome': 'Produto 4', 'preco': 4},
+    {"nome": "Produto 5", "preco": 10},
+    {"nome": "Produto 1", "preco": 22},
+    {"nome": "Produto 3", "preco": 2},
+    {"nome": "Produto 2", "preco": 6},
+    {"nome": "Produto 4", "preco": 4},
+    {"nome": "Produto 6", "preco": 52},
 ]
 
 
@@ -17,13 +18,9 @@ produtos = [
 #     return acumulador + produto['preco']
 
 
-total = reduce(
-    lambda ac, p: ac + p['preco'],
-    produtos,
-    0
-)
+total = reduce(lambda ac, p: ac + p["preco"], produtos, 0)
 
-print('Total é', total)
+print("Total é", total)
 
 
 # total = 0
