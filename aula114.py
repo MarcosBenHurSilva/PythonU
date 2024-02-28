48f2515d2b261ff973f78d588bd28aa1a9250bde
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-02-27 21:40:22 -0300

    Funções recursivas e recursividade

--- aula114.py
+++ aula114.py
@@ -36,7 +36,11 @@ def factorial(n):
     return n * factorial(n - 1)
 
 
+print(factorial(0))
+print(factorial(1))
 print(factorial(2))
+print(factorial(3))
+print(factorial(4))
 print(factorial(5))
 print(factorial(10))
 print(factorial(100))
