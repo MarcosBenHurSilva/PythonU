de80d2eec08bcfa188a4b9d4443413f9c754deda
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-03-05 21:59:25 -0300

    Modos de abertura e encoding com with open

--- aula116.py
+++ aula116.py
@@ -55,6 +55,7 @@ with open(caminho_arquivo, "r") as arquivo:
 with open(caminho_arquivo, "w", encoding="utf8") as arquivo:
     arquivo.write("Testando\n")
     arquivo.write("Atenção\n")
+    arquivo.write("Linha 0\n")
     arquivo.write("Linha 1\n")
     arquivo.write("Linha 2\n")
     arquivo.writelines(("Linha 3\n", "Linha 4\n"))
