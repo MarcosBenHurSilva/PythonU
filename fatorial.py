b46fb42a56d09dbc8bcee4bf16dc18cf94db11ae
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-03-17 01:11:58 -0300

    fatorial

--- fatorial.py
+++ fatorial.py
@@ -1,13 +1,14 @@
 def fat(n):
-    if n < 0:
-        print("Não existe fatorial de número negativo!")
-    elif n == 0:
+    if n == 0:
         return 1
+    elif n < 0:
+        print("Não existe fatorial de número negativo!")
     else:
         return n * fat(n - 1)
 
 
 if __name__ == "__main__":
-    n = -5
+    n = 2
     res = fat(n)
-    print(f"O fatorial de {n} é {res}")
+    if n >= 0:
+        print(f"O fatorial de {n} é {res}")
