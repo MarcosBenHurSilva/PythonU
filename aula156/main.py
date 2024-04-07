0e3882c0eee0123b9dc9c745bad1b13f6fe46f93
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-04-07 03:40:40 -0300

    DocStrings de várias linhas

--- aula156/main.py
+++ aula156/main.py
@@ -5,3 +5,13 @@ import uma_linha
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
 help(uma_linha)
+
+##################################
+
+import varias_linhas
+
+# print(dir(uma_linha))
+# print(uma_linha.__doc__)
+# print(uma_linha.__file__)
+# print(uma_linha.__name__)
+help(varias_linhas)
