37988541aa7af7c43d3a168d8061167dfc6e19f2
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-04-08 04:02:25 -0300

    DocStrings em funções (Documentação)

--- aula156/main.py
+++ aula156/main.py
@@ -1,4 +1,5 @@
 import uma_linha
+import documentando_funcoes
 
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
@@ -15,3 +16,4 @@ import varias_linhas
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
 help(varias_linhas)
+help(documentando_funcoes)
