030c073debf66af249e654ac85429a92d2704eaf
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-04-18 23:19:08 -0300

    Valores padrão e field em dataclasses

--- aula159.py
+++ aula159.py
@@ -5,34 +5,40 @@
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import asdict, astuple, dataclass
+# Valores padrão e field em dataclasses
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Pessoa:
-    nome: str
-    # idade: int
-    sobrenome: str
+    # nome: str
+    # # idade: int
+    # sobrenome: str
+    nome: str = field(default="MISSING", repr=False)
+    sobrenome: str = "Not sent"
+    idade: int = 100
+    enderecos: list[str] = field(default_factory=list)
 
-    #     @property
-    #     def nome_completo(self):
-    #         return f"{self.nome} {self.sobrenome}"
 
-    #     @nome_completo.setter
-    #     def nome_completo(self, valor):
-    #         nome, *sobrenome = valor.split()
-    #         self.nome = nome
-    #         self.sobrenome = " ".join(sobrenome)
+#     @property
+#     def nome_completo(self):
+#         return f"{self.nome} {self.sobrenome}"
 
-    #     def __init__(self, nome, idade, sobrenome):
-    #         self.nome = nome
-    #         self.idade = idade
-    #         self.sobrenome = " ".join(sobrenome)
-    #         self.sobrenome = sobrenome
-    #         self.nome_completo = f"{self.nome} {self.sobrenome}"
+#     @nome_completo.setter
+#     def nome_completo(self, valor):
+#         nome, *sobrenome = valor.split()
+#         self.nome = nome
+#         self.sobrenome = " ".join(sobrenome)
 
-    #     def __post_init__(self):
-    #         print("POST INIT")
+#     def __init__(self, nome, idade, sobrenome):
+#         self.nome = nome
+#         self.idade = idade
+#         self.sobrenome = " ".join(sobrenome)
+#         self.sobrenome = sobrenome
+#         self.nome_completo = f"{self.nome} {self.sobrenome}"
+
+#     def __post_init__(self):
+#         print("POST INIT")
 
 
 if __name__ == "__main__":
@@ -46,8 +52,11 @@ if __name__ == "__main__":
     # lista = [Pessoa("A", "Z"), Pessoa("B", "Y"), Pessoa("C", "X")]
     # ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
     # print(ordenadas)
-    p1 = Pessoa("Luiz", "Otávio")
-    print(asdict(p1).keys())
-    print(asdict(p1).values())
-    print(asdict(p1).items())
-    print(astuple(p1)[0])
+    # p1 = Pessoa("Luiz", "Otávio")
+    # print(asdict(p1).keys())
+    # print(asdict(p1).values())
+    # print(asdict(p1).items())
+    # print(astuple(p1)[0])
+    p1 = Pessoa()
+    # print(fields(p1))
+    print(p1)
